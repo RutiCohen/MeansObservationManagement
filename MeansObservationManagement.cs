aa2ffbeebf8045244aada2c8e25a7dab1c4dfb20
RutiCohen <48654169+RutiCohen@users.noreply.github.com>
2022-07-18 21:56:13 +0300

    Create MeansObservationManagement
    conatin all file you need

---
+++ MeansObservationManagement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagementOfObservation.Models.Entities;
+using ManagementOfObservation.Models.Enums;
+
+namespace ManagementOfObservation
+{
+    public class MeansObservationManagement
+    {
+        private static List<MeansObservation> means  = new List<MeansObservation>();
+
+        public static bool AddMeansObservation (MeansObservation item)
+        {
+            if(means.Any(x=>x.TypesMeansObservation == item.TypesMeansObservation 
+                            && (x.Range.Meters == item.Range.Meters || x.Range.AerialLine == item.Range.AerialLine)))
+            {
+                Console.WriteLine("There is exists item with same TypesMeansObservation && metters or AerialLine");
+                return false;
+            }   
+            if(means.Any(x => x.TypesMeansObservation == item.TypesMeansObservation
+                            && (x.FieldOfView.Degrees == item.FieldOfView.Degrees || x.FieldOfView.Angle == item.FieldOfView.Angle)))
+            {
+                Console.WriteLine("There is exists item with same TypesMeansObservation && Degrees or Angle");
+                return false;
+            }
+            Console.WriteLine("Item Added Successfully");
+            means.Add(item);
+            return true;
+        }
+
+        public static bool RemoveMeansObservation(MeansObservation item)
+        {
+            int itemIndexToRemove = means.FindIndex(x => x.TypesMeansObservation == item.TypesMeansObservation
+                            && (x.Range.Meters == item.Range.Meters && x.Range.AerialLine == item.Range.AerialLine)
+                            && (x.FieldOfView.Degrees == item.FieldOfView.Degrees || x.FieldOfView.Angle == item.FieldOfView.Angle));
+            if (itemIndexToRemove != -1)
+            {
+                means.RemoveAt(itemIndexToRemove);
+                Console.WriteLine("Item removed successfully");
+                return true;
+            }
+            Console.WriteLine("Failed remove item, check if item exists...");
+            return false;
+        }
+
+        public static List<MeansObservation> GetAllMeansObservation()
+        {
+            return means;
+        }
+
+        public static List<MeansObservation> GetMeansObservation(TypesMeansObservation type)
+        {
+            return means?.FindAll(x=>x.TypesMeansObservation == type);
+        }
+
+        public static List<MeansObservation> GetOrderingMeansObservation()
+        {
+            return means?.OrderBy(x=>x.Range.Meters).ThenBy(x => x.Range.AerialLine).ToList<MeansObservation>();
+        }
+
+        public static MeansObservation GetMaxMeansObservation(FieldOfView minFieldOfView)
+        {
+            List<MeansObservation> list = means.FindAll(x => x.FieldOfView.Angle >= minFieldOfView.Angle && x.FieldOfView.Degrees >= minFieldOfView.Degrees);
+            return list?.Count < 2 ? list.FirstOrDefault() : list?.Aggregate((a, b) => a.Range.Meters > b.Range.Meters ? a : b); 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using ManagementOfObservation.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagementOfObservation.Models.Entities;
+
+namespace ManagementOfObservation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to ManagementOfObservation");
+            Console.WriteLine("Please enter MeansObservation");
+            AddMeansObservation();
+            Console.WriteLine("\n-------Remove MeansObservation");
+            Console.WriteLine("Please enter MeansObservation details to remove");
+            MeansObservation item = MeansObservation.Init();
+            if (item == null)
+                return;
+            MeansObservationManagement.RemoveMeansObservation(item);
+            Console.WriteLine("\n-------Display all MeansObservation");
+            List<MeansObservation> all = MeansObservationManagement.GetAllMeansObservation();
+            if (!all.Any())
+                Console.WriteLine("Not found any MeansObservation");
+            else
+            {
+                foreach(MeansObservation meansObservation in all)
+                {
+                    meansObservation.Details();
+                }
+            }
+            Console.WriteLine("\n-------GetTypesMeansObservation");
+            Console.WriteLine("Enter TypesMeansObservation:");
+            int TypesMeansObservation = MeansObservation.GetTypesMeansObservation();
+            if(TypesMeansObservation == -1)
+                Console.WriteLine("not valid TypesMeansObservation to search");
+            else
+            {
+                all = MeansObservationManagement.GetMeansObservation((TypesMeansObservation)TypesMeansObservation);
+                if (!all.Any())
+                    Console.WriteLine("Not found any MeansObservation");
+                else
+                {
+                    foreach (MeansObservation meansObservation in all)
+                    {
+                        meansObservation.Details();
+                    }
+                }
+            }
+            Console.WriteLine("\n-------GetOrderingMeansObservation");
+            all = MeansObservationManagement.GetOrderingMeansObservation();
+            if (!all.Any())
+                Console.WriteLine("Not found any MeansObservation");
+            else
+            {
+                foreach (MeansObservation meansObservation in all)
+                {
+                    meansObservation.Details();
+                }
+            }
+            Console.WriteLine("\n-------GetMaxMeansObservation");
+            FieldOfView FieldOfView = FieldOfView.Init();
+            if(FieldOfView == null)
+                Console.WriteLine("not valid FieldOfView to search");
+            MeansObservation MaxMeansObservation = MeansObservationManagement.GetMaxMeansObservation(FieldOfView);
+            if (MaxMeansObservation == null)
+                Console.WriteLine("not found MaxMeansObservation");
+            else
+                MaxMeansObservation.Details();
+            Console.WriteLine("END");
+            Console.ReadLine();
+        }
+
+        static void AddMeansObservation()
+        {
+            while (true)
+            {
+                MeansObservation item = MeansObservation.Init();
+                if (item == null)
+                    return;
+                MeansObservationManagement.AddMeansObservation(item);
+            }
+        }
+    }
+}
